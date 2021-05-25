c2d922dae132c62efd839dc69304ce4ace225d84
b2wu <81795401+b2wu@users.noreply.github.com>
2021-05-24 17:10:23 -0700

    Update router.js

--- scripts/router.js
+++ scripts/router.js
@@ -1,70 +1,91 @@
 // router.js
-
 export const router = {};
 
+const body = document.body;
+const headerText = document.querySelector('header > h1');
+let main = document.querySelector('main');
+let singleEntry = document.querySelector('entry-page');
+
 /**
- * Changes the "page" (state) that your SPA app is currently set to
+ * Set the state for the new page
+ * @param {string} state The new page to set the state of
+ * @param {boolean} statePopped If the request came from a popstate event
+ * @param {number} entryNum if state is 'entry', then entryNum is the num
  */
-router.setState = function(state, flag) {
-  /**
-   * - There are three states that your SPA app will have
-   *    1. The home page
-   *    2. The entry page (showing one individual entry)
-   *    3. The settings page (currently blank, no actual settings here, just a placeholder where a real settings page would go)
-   * 
-   * - If you look at the CSS, we have 2 classes you can add to the body element to help change states, "settings" and "single-entry"
-   * - Changing states will require more than just changing these classes, for example the settings page requires you to change the title to "Settings"
-   * - And each individual entry the title changes to "Entry #" based on it's number in the entry order
-   *
-   * - When changing states, make sure the back and forward buttons work. You can use hash URLs (e.g. https://someurl.com/#settings) when changing states
-   *   to make things easier.
-   * - Similarly, when viewing an individual entry, a hashed URL might look like https://someurl.com/#entry3
-   * 
-   * - Some tips:
-   *    1. Push a new state object to the history object using history.pushState() 
-   *    2. look up the documentation for how to use pushState() when you try it
-   *    3. look up the documentation for the "popstate" event listener (fires only on back button), useful in your script.js file
-   *    4. For each <journal-entry> element, you can grab the JSON version of its info with .entry (e.g. someJournalEntryElement.entry)
-   *       a. This is useful when viewing a single entry. You may notice an <entry-page> element in the HTML, this is the element that is displayed when the
-   *          .single-entry class is applied to the body. You can populate this element by using .entry similarly. So if I wanted to grab a specific <journal-entry>
-   *          and populate it's info into the <entry-page>, I would simply use an assignment of newEntryElement.entry = journalEntryElement.entry
-   *       b. Clearing the <entry-page> element of its previous data can be a bit tricky, it might be useful to just delete it and insert a new blank one 
-   *          in the same spot each time. Just a thought.
-   *
-   * - Answers to some questions you may have:
-   *    1. You may add as many helper functions in this file as you like
-   *    2. You may modify the parameters of setState() as much as you like
-   */
-  let body = document.querySelector("body");
-  let title = document.querySelector("h1");
+router.setState = (state, statePopped, entryNum) => {
+  switch (state) {
+    case 'settings':
+      entryPage(false);
+      settingsPage(true);
+      break;
+    case 'entry':
+      entryPage(true, entryNum);
+      break;
+    default:
+      homePage();
+  }
 
-  if(state.name == 'main') {
-    title.innerText = 'Jounral Entries';
+  if(!statePopped && window.location.hash != `#${state}`) {
+    pushToHistory(state, entryNum);
+  }
+}
+
+/**
+ * @param {boolean} goTo true if you are going to this page, false if unsetting the page
+ */
+function settingsPage(goTo) {
+  if (goTo) {
+    body.classList.add('settings');
+    headerText.innerHTML = 'Settings';
+  } else {
     body.classList.remove('settings');
-    body.classList.remove('single-entry');
-    if(!flag){
-      history.pushState(state,'', location.origin);
-    }
+    headerText.innerHTML = 'Journal Entries';
   }
-  else if(state.name == 'settings'){
-    title.innerText = 'Settings';
-    body.className = 'settings';
-    if(!flag){
-      history.pushState(state, '', '#settings');
-    }
-  }
-  else if(state.name == 'entry'){
-    let id = state.id;
-    title.innerText= 'Entry' + id;
-    body.className = 'single-entry';
-    let newEntry = document.querySelector('entry-page');
-    newEntry.remove();
-    newEntry = document.createElement('entry-page');
-    newEntry.entry = document.getElementById(id).entry;
-    body.appendChild(newEntry);
-    if(!flag){
-      history.pushState(state,'', '#entry' + id);
-    }
-  }
+}
 
+/**
+ * 
+ * @param {boolean} goTo  true if you are going to this page, false if unsetting the page
+ * @param {number} entryNum if goTo is true, number of the entry to render
+ */
+function entryPage(goTo, entryNum) {
+  if (goTo) {
+    body.classList.remove('settings');
+    singleEntry.parentNode.removeChild(singleEntry);
+    singleEntry = document.createElement('entry-page');
+    body.classList.add('single-entry');
+    headerText.innerHTML = `Entry ${entryNum}`;
+    singleEntry.entry = Array.from(document.querySelector('main').childNodes)[entryNum - 1].entry;
+    main.insertAdjacentElement('afterend', singleEntry);
+  } else {
+    body.classList.remove('single-entry');
+    headerText.innerHTML = 'Journal Entries';
+  }
+}
+
+/**
+ * Removes any other set pages and goes back to home page
+ */
+function homePage() {
+  settingsPage(false);
+  entryPage(false);
+}
+
+/**
+ * Push a new state to the history stack
+ * @param {string} state The new page to set the state of
+ * @param {number} entryNum if state is 'entry', then entryNum is the num
+*/
+export function pushToHistory(state, entryNum) {
+  switch (state) {
+    case 'settings':
+      history.pushState({ page: 'settings' }, '', './#settings');
+      break;
+    case 'entry':
+      history.pushState({ page: `entry${entryNum}` }, '', `./#entry${entryNum}`);
+      break;
+    default:
+      history.pushState({}, '', './');
+  }
+  return history;
 }
